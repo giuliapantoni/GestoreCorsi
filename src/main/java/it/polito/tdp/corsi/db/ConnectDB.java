1ac5b9db281bea9c9ba6ea91858b967390b0582a
710490 <710490@DESKTOP-AC0KD4F.homenet.telecomitalia.it>
2020-04-14 14:57:40 +0200

    SOLUZIONE

---
+++ src/main/java/it/polito/tdp/corsi/db/ConnectDB.java
@@ -0,0 +1,12 @@
+package it.polito.tdp.corsi.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	public static Connection getConnection() throws SQLException {
+		String jdbcURL = "jdbc:mysql://localhost/iscritticorsi?user=root&password=ste28fa08no93";
+		return DriverManager.getConnection(jdbcURL);
+	}
+}
---
+++ src/main/java/it/polito/tdp/corsi/db/CorsoDAO.java
@@ -0,0 +1,148 @@
+package it.polito.tdp.corsi.db;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.corsi.model.Corso;
+import it.polito.tdp.corsi.model.Studente;
+
+public class CorsoDAO {
+
+	public boolean esisteCorso(String codins) {
+		String sql = "SELECT * FROM corso WHERE codins = ?";
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, codins);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				conn.close();
+				return true;
+			} else {
+				conn.close();
+				return false;
+			}
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	
+	}
+	
+	public List<Corso> getCorsiByPeriodo(int pd){
+		
+		String sql = "SELECT *  FROM corso WHERE pd = ?";
+		List<Corso> result = new ArrayList<Corso>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, pd);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				Corso c = new Corso(rs.getString("codins"), rs.getInt("crediti"), rs.getString("nome"), rs.getInt("pd"));
+				result.add(c);
+			}
+			conn.close();
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	
+	public Map<Corso, Integer> getIscrittiByPeriodo(int pd){
+		String sql = "SELECT c.codins, c.nome, c.crediti, c.pd, COUNT(*) AS tot " + 
+				"FROM corso AS c, iscrizione as i " + 
+				"WHERE c.pd = ? and c.codins = i.codins " + 
+				"GROUP BY c.codins, c.nome, c.crediti, c.pd " ;
+		Map<Corso, Integer> result = new HashMap<Corso, Integer>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, pd);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				Corso c = new Corso(rs.getString("codins"), rs.getInt("crediti"), rs.getString("nome"), rs.getInt("pd"));
+				int n = rs.getInt("tot");
+				result.put(c, n);
+			}
+			
+			conn.close();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	
+	public List<Studente> getStudentiByCorso(Corso corso){
+		String sql = "SELECT s.matricola, s.nome, s.cognome, s.CDS " + 
+				"FROM studente AS s, iscrizione AS i " + 
+				"WHERE s.matricola = i.matricola AND i.codins = ?";
+	
+		List<Studente> studenti = new LinkedList<Studente>();
+	
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, corso.getCodins());
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				Studente s = new Studente(rs.getInt("matricola"), rs.getString("nome"), rs.getString("cognome"), rs.getString("CDS"));
+				studenti.add(s);
+			}
+			conn.close();
+		}catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	
+		return studenti;
+	
+	}
+	
+	public Map<String, Integer> getDivisioneCDS(Corso c){
+		String sql = "SELECT s.CDS, COUNT(*) AS tot " + 
+				"FROM studente AS s, iscrizione AS i " + 
+				"WHERE s.matricola = i.matricola AND s.CDS <> \"\" AND i.codins = ? " + 
+				"GROUP BY s.CDS ";
+		
+		Map<String, Integer> statistiche = new HashMap<String, Integer>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, c.getCodins());
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				statistiche.put(rs.getString("CDS"), rs.getInt("tot"));
+			}
+			conn.close();
+		}catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return statistiche;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
